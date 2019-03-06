35df417bfca80275ef1e63e83252b9d1c1ff0559
guozi2 <lewoking@gmail.com>
2019-03-06 15:05:28 +0800

    done  it's ok

--- ftp_check.py
+++ ftp_check.py
@@ -14,31 +14,38 @@ import ftplib
 
 
 
-def run_command(items):
+pypath = os.getcwd()
+with open(pypath +'\host.ini', 'r') as host_info:
+    for line in host_info.readlines():
+
+        items = line.split("#")
+
     server=items[0]
     user=items[1]
     password=items[2]
     check_dir=items[3]
     file_head =items[4]
     file_end =items[5]
-    gap = items[5]
+    gap = items[6]
+    sp = ""
 
     ftp = ftplib.FTP(server)
     ftp.login(user,password)
-    if gap == 10 :
+    if gap == "10":
         mtime = [ str(x).zfill(2) + str(y).zfill(2) for x in range(0,24) for y in range(0,60,10)]
-    elif gap == 5 :
+    elif gap =="5":
         mtime = [ str(x).zfill(2) + str(y).zfill(2) for x in range(0,24) for y in range(0,60,5)]
     else:
         print ('time error')
+
     for z in mtime:
         sp = sp + file_head + time.strftime('%Y%m%d',time.localtime(time.time()))+ z + file_end +" "
-    needfile = sp.split()
+    need_file = sp.split()
 
     files = []
     ftp.cwd(check_dir)
     files = ftp.nlst()
-    ftp.quit()
+
 
 
 def diff(listA, listB):
@@ -59,10 +66,4 @@ def main():
     diff(listA, listB)
 
 if __name__ == '__main__':
-    pypath = os.getcwd()
-    with open(pypath +'\host.ini', 'r') as host_info:
-        for line in host_info.readlines():
-            if line[0:1] == '#':continue
-            items = line.split()
-            run_command(items)
     main()
