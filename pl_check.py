43a3a2b40eefa90e000527d756294a1a9c7d71e7
lewoking <lewoking@gmail.com>
2020-09-03 01:33:32 +0800

    try error

--- pl_check.py
+++ pl_check.py
@@ -11,9 +11,8 @@ import sys
 
 def header(work_sheet):
     work_sheet.write(0, 0,'检查主机',style0)
-    work_sheet.write(0, 1, '项目', style0)
-    work_sheet.write(0, 2, '执行命令', style0)
-    work_sheet.write(0, 3, '检查结果', style0)
+    work_sheet.write(0, 1, '执行命令', style0)
+    work_sheet.write(0, 2, '检查结果', style0)
     return work_sheet
 
 def run(text):
@@ -29,29 +28,32 @@ def run_command(item):
     except:
         print ("Command conf error,please check cfg file.",item[4])
         exit(1)
-    work_sheet = work_book.add_sheet(item[0])
-    work_sheet = header(work_sheet)
+    # work_sheet = work_book.add_sheet(item[0])
+    # work_sheet = header(work_sheet)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         ssh.connect(host,int(port),user,passwd,timeout=5)
-        for cmd in cmds.readlines():
-            rows = len(work_sheet.rows)
-            items = cmd.split('#')
-            check_type = items[0].encode('utf-8')
-            command = items[1].encode('utf-8')
-            stdin, stdout, stderr = ssh.exec_command(command.decode('utf-8'))
-            return_info = stdout.read().strip()
-            work_sheet.write(rows,0,host)
-            work_sheet.write(rows,1,items[0])
-            work_sheet.write(rows,2,items[1])
-            work_sheet.write(rows,3,return_info.decode('utf-8'))
-        print ('host command success :',host)
     except:
         print ("ssh connect timeout,please check network connect.",item[0])
+    for cmd in cmds.readlines():
+        items = cmd.split('#')
+        try:
+            work_sheet = work_book.add_sheet(items[0])
+        except:
+             print ("duplicate worksheet name.",items[0])
+
+        work_sheet = work_book.sheet_by_name(items[0])
         rows = len(work_sheet.rows)
+        check_type = items[0].encode('utf-8')
+        command = items[1].encode('utf-8')
+        stdin, stdout, stderr = ssh.exec_command(command.decode('utf-8'))
+        return_info = stdout.read().strip()
+
         work_sheet.write(rows,0,host)
-        work_sheet.write(rows,1,"ssh connect timeout,please check network connect.")
+        work_sheet.write(rows,1,items[1])
+        work_sheet.write(rows,2,return_info.decode('utf-8'))
+    print ('host command success :',host)
     cmds.close()
 
 if __name__ == '__main__':
